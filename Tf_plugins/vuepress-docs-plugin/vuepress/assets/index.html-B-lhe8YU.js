ebc044347e0d4caa405cb23ccd1ade1e65157848
Tivonfeng <tivonfeng@163.com>
2026-08-21 22:23:07 +0800

    docs(vuepress): 更新教程站点构建产物
    重新构建的静态页面与 hash 资源（127 个页面更新，旧资源清理）

---
+++ Tf_plugins/vuepress-docs-plugin/vuepress/assets/index.html-B-lhe8YU.js
@@ -0,0 +1,16 @@
+import{_ as n}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as i,d as c,a as t,o}from"./app-C2OgZUwN.js";const a="/docs/assets/1-BwFd9Uhu.png",r={};function s(l,e){return o(),i("div",null,[e[0]||(e[0]=c('<h1 id="🎉-欢迎进入scratch的世界" tabindex="-1"><a class="header-anchor" href="#🎉-欢迎进入scratch的世界"><span>🎉 欢迎进入Scratch的世界</span></a></h1><figure><img src="'+a+'" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><div class="hint-container tip"><p class="hint-container-title">欢迎</p><p>哈喽大家好！我是冯布斯！ 欢迎各位跟我一起进入<code>Scratch</code>的世界！</p></div><div class="hint-container tip"><p class="hint-container-title">课程 &amp;&amp; 教学系统</p><ul><li>本套课程体系由 <code>绿旗冯布斯老师</code>和<code>绿旗木木老师</code>设计开发，对标中国电子学会编程等级考试1-4个级别！</li><li>课程结合 <code>绿旗在线Scratch编程</code> 系统，可以边学习边实操，真正锻炼编程实战能力！</li><li><a href="http://asset.lqcode.fun/TF_training_scratch.pdf" target="_blank" rel="noopener noreferrer">配套训练体系表格下载</a>，PDF文件密码<code>lqcode</code></li></ul></div><div class="hint-container warning"><p class="hint-container-title">开发中</p><p>课程暂未开发完成，请耐心等待！</p></div>',5)),t(` <VPBanner
+  title="Mr.Hope"
+  content="Where there is light, there is hope"
+  logo="https://mister-hope.com/logo.svg"
+  :actions='[
+    {
+      text: "访问",
+      link:"https://mister-hope.com",
+    },
+    {
+      text: "仓库",
+      link: "https://github/Mister-Hope/Mister-Hope.github.io",
+      type: "default",
+    },
+  ]'
+/> `),t(" <PDF url='http://asset.lqcode.fun/scratch_block.pdf'/> ")])}const h=n(r,[["render",s]]),m=JSON.parse('{"path":"/docs/scratch/","title":"🎉 欢迎进入Scratch的世界","lang":"zh-CN","frontmatter":{"comment":false},"git":{"createdTime":1698335100000,"updatedTime":1699701050000,"contributors":[{"name":"Tivonfeng","username":"Tivonfeng","email":"tivonfeng@163.com","commits":9,"url":"https://github.com/Tivonfeng"}]},"readingTime":{"minutes":0.65,"words":194},"filePathRelative":"docs/scratch/index.md","excerpt":"\\n<figure><figcaption></figcaption></figure>\\n<div class=\\"hint-container tip\\">\\n<p class=\\"hint-container-title\\">欢迎</p>\\n<p>哈喽大家好！我是冯布斯！\\n欢迎各位跟我一起进入<code>Scratch</code>的世界！</p>\\n</div>\\n<div class=\\"hint-container tip\\">\\n<p class=\\"hint-container-title\\">课程 &amp;&amp; 教学系统</p>\\n<ul>\\n<li>本套课程体系由 <code>绿旗冯布斯老师</code>和<code>绿旗木木老师</code>设计开发，对标中国电子学会编程等级考试1-4个级别！</li>\\n<li>课程结合 <code>绿旗在线Scratch编程</code> 系统，可以边学习边实操，真正锻炼编程实战能力！</li>\\n<li><a href=\\"http://asset.lqcode.fun/TF_training_scratch.pdf\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">配套训练体系表格下载</a>，PDF文件密码<code>lqcode</code></li>\\n</ul>\\n</div>"}');export{h as comp,m as data};
